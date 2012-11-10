bcb1ef47e1f2b6ac70d5d37240fca6a727ab3c1c
Tomas Dragoun <tomas.dragoun@seznam.cz>
2012-11-10 13:15:35 +0100

    homework 07

--- 07/pb173.c
+++ 07/pb173.c
@@ -1,48 +1,48 @@
-#include <linux/io.h>
 #include <linux/module.h>
-#include <linux/delay.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/kfifo.h>
 
-static void beep(unsigned int hz)
-{
-	u8 enable;
+/* declare static 8KB char fifo */
+DECLARE_KFIFO(my_fifo, char, 8192);
 
-	if (!hz) {
-		enable = 0x00;		/* Turn off speaker */
-	} else {
-		u16 div = 1193181/hz;
-
-		outb(0xb6, 0x43);	/* Ctr 2, squarewave, load, binary */
-		udelay(10);
-		outb(div, 0x42);	/* LSB of counter */
-		udelay(10);
-		outb(div >> 8, 0x42);	/* MSB of counter */
-		udelay(10);
-
-		enable = 0x03;		/* Turn on speaker */
-	}
-	inb(0x61);		/* Dummy read of System Control Port B */
-	udelay(10);
-	outb(enable, 0x61);	/* Enable timer 2 output to speaker */
-	udelay(10);
+ssize_t my_write(struct file *filp, const char __user *buf, size_t count, loff_t *off){
+	int nbytes;
+	/* copy user buffer to fifo */
+	kfifo_from_user(&my_fifo, buf, count, &nbytes);
+	return nbytes;
 }
+
+ssize_t my_read(struct file *filp, char __user *buf, size_t count, loff_t *off){
+	int nbytes;
+	/* copy data from fifo to user */
+	kfifo_to_user(&my_fifo, buf, count, &nbytes);
+	return nbytes;
+}
+
+struct file_operations my_fops = {
+	.owner = THIS_MODULE,
+	.write = my_write,
+	.read = my_read,
+};
+
+struct miscdevice misc = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "my_fifo_device",
+	.fops = &my_fops,
+};
 
 static int my_init(void)
 {
-	beep(1000);
-	msleep(500);
-	beep(1500);
-	msleep(500);
-	beep(1800);
-	msleep(500);
-	beep(300);
-	msleep(500);
-	beep(0);
-
-	return -EIO;
+	misc_register(&misc);
+	/* initialize fifo  */
+	INIT_KFIFO(my_fifo);
+	return 0;
 }
 
 static void my_exit(void)
 {
+	misc_deregister(&misc);
 }
 
 module_init(my_init);
